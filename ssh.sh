260d7248b45de47881455358373df54f2cd4e0bb
Vladislav Soprun <develop@soprun.com>
2020-04-16 19:40:51 +0300

    commit

--- ssh.sh
+++ ssh.sh
@@ -1,153 +1,72 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -u
-# set -eu -o pipefail
-
-#if read -t 2 -p "Enter your name: " name
-#
-#then
-#  echo "Hello $name, welcome to my script"
-#else
-#  echo "Sorry, too slow! "
-#fi
-#
-#exit;
-
-
-if [[ $# -eq 0 ]]; then
-    echo -e "An error occurred, empty parameters."
-    exit 1;
-fi
-
-
-
-#[[ -n "${1:-}" ]]
-
-#if [[ "${1:-unset}" == "unset" ]]; then
-#    echo -e "No parameters found. "
-#    exit 1
-#fi
-
-# [name] Имя пользователя для подключения к экземпляру.
-# name="develop@soprun.com";
-# hostname="soprun.com";
-
-# [filename] это имя, которое вы хотите использовать для файлов ключей SSH.
-# filename="id-${name}";
-# Public key file: ~/.ssh/${filename}.pub
-# key key file: ~/.ssh/${filename}
-
-# filename="$HOME/.ssh/${filename}";
-
-# [expire_time] is a value in ISO 8601 format. For example: 2018-12-04T20:12:00+0000.
-# expire_time="";
-# key_passphrase="";
-
-#rm -f \
-#  "${key_file}" \
-#  "${key_file}.pub" \
-#  "~/.ssh/known_hosts"
+#set -ue
+set -eu -o pipefail
 
 # identity="develop@soprun.com"
 # hostname="github.com"
 # remote_host="github.com"
 
-
 identity=${1:-"develop@soprun.com"}
 
-#if [[ -n "${0}" ]]; then
-#  echo "No parameters found."
-#fi
+echo "Enter identity [${identity}]: "
 
-echo "Hello 👀: ${identity}"
-
-exit;
-
-#echo -e -n "Enter your identity [${identity}]: "
-#read identity_input
-#
-#identity=${identity_input:-${identity}}
-#
-#echo -e "\n"
-
-
-# identity
-
-# pkill ssh-agent;
-# killall ssh-agent;
-
-rm -rf ~/.ssh/*;
-
-# mkdir -p ~/.ssh
-# mkdir -p "${HOME}/.ssh"
-# chmod 0700 "${HOME}/.ssh"
-
-# ssh $1 "googel.com"
-# ssh $1 "uptime"
-
-known_hosts="${HOME}/.ssh/known_hosts";
-
-# SSH fingerprints
-if [[ ! -f ${known_hosts} ]]; then
-  echo -e "Create file: ${known_hosts}";
-  touch ${known_hosts};
+if read -t 0 identity_user; then
+  identity=${identity_user};
 fi
 
-# ssh-keyscan -H 192.168.1.162 >> ~/.ssh/known_hosts
+#rm -rf ~/.ssh/*
+
+mkdir -p "${HOME}/.ssh"
+chmod 0700 "${HOME}/.ssh"
 
 # echo "Authentication key generation!"
-
-declare -ir bits=2048
-declare -r type="rsa"
-
-passphrase=''
 
 filename="${HOME}/.ssh/${identity}_rsa"
 key_public="${filename}.pub"
 key_private="${filename}"
 
 if [[ ! -f ${filename} ]]; then
-    echo -e "\nGenerating an ${type}-${bits} bit key for \"${identity}\"";
+  declare -ir bits=2048
+  declare -r type="rsa"
+  declare passphrase="secret-${identity}";
+  # echo "Wait!.."
+  # sleep 2;
 
-    # read -s -p "Enter your passphrase: " ${passphrase}
-    # echo "Is your password really ${passphrase}? "
+  echo "Enter passphrase [${passphrase}]: "
+  if read -t 0 -s passphrase_user; then
+    passphrase=${passphrase_user};
+  fi
 
-    ssh-keygen \
-      -q \
-      -b ${bits} \
-      -t ${type} \
-      -N ${passphrase} \
-      -C ${identity} \
-      -f ${filename}
+  clear;
+  echo -e "Generating an ${type}-${bits} bit key for \"${identity}\"";
 
-    chmod 400 ${filename};
+  ssh-keygen \
+    -q \
+    -b ${bits} \
+    -t ${type} \
+    -N ${passphrase} \
+    -C ${identity} \
+    -f ${filename}
+
+  chmod 400 ${filename};
+
+  echo -e "\t- public key: ${key_public}"
+  echo -e "\t- private key: ${key_private}"
 fi
 
-# echo "Identity: ${identity}"
-#
-#die(){
-#  echo "$1" 2>&1
-#}
+echo "";
+exec pbcopy < ${key_public} | pbpaste
+echo "";
 
-#if [[ -f ${key_public} ]]; then
-##  echo -e "${key_public}"
-#  # die "File not found";
-#fi
+if [[ ! -z ${SSH_AGENT_PID:-} ]]; then
+  eval $(ssh-agent -s) > /dev/null
+fi;
 
-echo -e "\tPublic key: ${key_public}"
+# ssh-add -L ${key_private}
 
-
-# echo "Private key: ${key_private}"
-
-# pbcopy < ${key_public};
-# ssh-keyscan -H 192.168.1.162 >> ~/.ssh/known_hosts
-
-#ssh-add -A;
-#ssh-add -L;
-
-#ssh-add -L -f ${filename}
-# echo -e "-----"
-# ssh -T git@github.com
+# ssh -i ${key_public} -T git@gitlab.com
 
 echo "Finished.."
+# Hi soprun! You've successfully authenticated, but GitHub does not provide shell access.
 exit 0;
