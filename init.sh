c7886de38164b7801f27a947405996093f726d3b
Vladislav Soprun <develop@soprun.com>
2020-04-16 20:50:16 +0300

    commit

--- init.sh
+++ init.sh
@@ -3,13 +3,4 @@
 ln -sf "$PWD/.bash_profile" "$HOME/.bash_profile"
 ln -sf "$PWD/.bash_aliases" "$HOME/.bash_aliases"
 ln -sf "$PWD/git.conf" "$HOME/.gitconfig"
-
-ssh_config_target="$HOME/.ssh/config"
-ssh_config_source="$PWD/ssh/config.conf"
-# ssh_options_target="$HOME/.ssh/options"
-# ssh_options_source="$PWD/ssh/options.conf"
-
-ln -sf ${ssh_config_source} ${ssh_config_target}
-# ln -sf ${ssh_options_source} ${ssh_options_target}
-chmod 600 ${ssh_config_target}
-# chmod 600 ${ssh_options_target}
+ln -sf "$PWD/ssh.conf" "$HOME/.ssh/config"
