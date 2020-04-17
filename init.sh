8e9bd164f341f716814dce18287e5b65049416d6
Vladislav Soprun <develop@soprun.com>
2020-04-18 00:17:22 +0300

    commit

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-ln -sf "$PWD/.bash_profile" "$HOME/.bash_profile"
+ln -sf "$PWD/bash/.bashrc" "$HOME/.bashrc"
+ln -sf "$PWD/bash/.profile" "$HOME/.profile"
+ln -sf "$PWD/bash/.bash_aliases" "$HOME/.bash_aliases"
+
 ln -sf "$PWD/git.conf" "$HOME/.gitconfig"
 ln -sf "$PWD/ssh.conf" "$HOME/.ssh/config"
 ln -sf "$PWD/gpg.conf" "$HOME/.gnupg/gpg.conf"
