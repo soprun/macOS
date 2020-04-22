b74182e1dd10c786800115070650418df583c711
Vladislav Soprun <develop@soprun.com>
2020-04-22 17:16:23 +0300

    commit

--- init.sh
+++ init.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
-# ln -sf "$PWD/bash/.bashrc" "$HOME/.bashrc"
-# ln -sf "$PWD/bash/.profile" "$HOME/.profile"
-# ln -sf "$PWD/bash/.bash_aliases" "$HOME/.bash_aliases"
+# .bash_profile config
+ln -sf "${PWD}/bash/.bash_profile" ~/.bash_profile
+ln -sf "${PWD}/bash/.bash_aliases" ~/.bash_aliases
+ln -sf "${PWD}/bash/.bashrc" ~/.bashrc
 
-ln -sf "$PWD/git/git.conf" "$HOME/.gitconfig"
-ln -sf "$PWD/ssh/ssh.conf" "$HOME/.ssh/config"
-ln -sf "$PWD/gpg/gpg.conf" "$HOME/.gnupg/gpg.conf"
-ln -sf "$PWD/gpg/dirmngr.conf" "$HOME/.gnupg/dirmngr.conf"
-ln -sf "$PWD/gpg/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"
+chmod 700 ~/.bash_profile
+chmod 700 ~/.bash_aliases
+chmod 700 ~/.bashrc
+
+# Git config
+ln -sf "$PWD/git/git.conf" ~/.gitconfig
+
+# SSH authentication
+ln -sf "$PWD/ssh/ssh.conf" ~/.ssh/config
+
+# GNU Privacy Guard
+ln -sf "$PWD/gpg/gpg.conf" ~/.gnupg/gpg.conf
+ln -sf "$PWD/gpg/dirmngr.conf" ~/.gnupg/dirmngr.conf
+ln -sf "$PWD/gpg/gpg-agent.conf" ~/.gnupg/gpg-agent.conf
 
 # exec "${PWD}/ssh-key-generation.sh"
 echo "You've successfully copy bash profiler."
