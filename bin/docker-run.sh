f5ab60451d4cc9aa3c3b39d4ac3a0af1c3de0846
Vladislav Soprun <develop@soprun.com>
2020-06-16 07:10:04 +0300

    commit...

---
+++ bin/docker-run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo ""
+echo ">> Starting docker-compose demon"
+echo ""
+
+docker-compose up -d
--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
+# todo: I think it's about signing through certificates.!!
 # git config --local gpg.x509.program smimesign
-
 
 git config --global gpg.format x509
 git config --global gpg.program gpg
