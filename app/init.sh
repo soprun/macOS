a89956f25cbfb9c5a1b7ec2b554f62432556c75f
Vladislav Soprun <develop@soprun.com>
2020-06-20 17:22:22 +0300

    commit...

--- app/init.sh
+++ app/init.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-# todo: I think it's about signing through certificates.!!
-# git config --local gpg.x509.program smimesign
-
-git config --global gpg.format x509
-git config --global gpg.program gpg
-git config --global commit.gpgsign true
-git config --global user.signingkey ${ID_GPG_KEY}
-
-pkill ssh-agent;
-pkill gpg-agent;
+# touch ~/.bash_profile;
+# open ~/.bash_profile
--- init.sh
+++ init.sh
@@ -3,10 +3,10 @@
 # todo: I think it's about signing through certificates.!!
 # git config --local gpg.x509.program smimesign
 
-git config --global gpg.format x509
-git config --global gpg.program gpg
-git config --global commit.gpgsign true
-git config --global user.signingkey ${ID_GPG_KEY}
+# git config --global gpg.format x509
+# git config --global gpg.program gpg
+# git config --global commit.gpgsign true
+# git config --global user.signingkey ${ID_GPG_KEY}
 
-pkill ssh-agent;
-pkill gpg-agent;
+# pkill ssh-agent;
+# pkill gpg-agent;
