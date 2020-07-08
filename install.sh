1703402952d848b21fefa2278c3a1238ce69cc2e
Vladislav Soprun <develop@soprun.com>
2020-07-09 00:04:54 +0300

    commit...

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+
 # https://scriptingosx.com/2017/04/about-bash_profile-and-bashrc-on-macos/
 
 # Enable tracing
